4df1a418238f709a740ddbf6ea64690f1d34c5b0
darkgreenth <darkgreenth@gmail.com>
2014-03-23 11:04:01 +0800

    up

---
+++ MyBusStop/src/com/blackdragoon/newtaipeicitybus/android/domain/dao/BaseVolleyListener.java
@@ -0,0 +1,10 @@
+package com.blackdragoon.newtaipeicitybus.android.domain.dao;
+
+import org.json.JSONArray;
+
+import com.android.volley.Response;
+
+public abstract class BaseVolleyListener implements
+		Response.Listener<JSONArray>, Response.ErrorListener {
+
+}
